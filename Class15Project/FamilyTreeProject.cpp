e26a1fc6fb6fbf2721f2621f7c07ba0aed983574
PaulDVS <91812462+PaulDVS@users.noreply.github.com>
2023-03-10 13:10:59 +0000

    Add files via upload

--- Class15Project/FamilyTreeProject.cpp
+++ Class15Project/FamilyTreeProject.cpp
@@ -85,8 +85,26 @@ class familyTree{
 			return Ancestor->checkId(Id);
 		}
 		
+		int getAge(int Id){
+			familyMemberNode *currNode = this->getMemberById(Id);
+			int age = currNode->getAge();
+		}
+		
 		void addSpouse(int Id, familyMemberNode *inputMember){
 			familyMemberNode *currNode = this->getMemberById(Id);
+			
+			//Checks spouse isn't younger than children
+			if(currNode->Child != NULL && currNode->Child->getAge() > inputMember->getAge()){
+				cout<<"You cannot marry someone younger than your children."<<endl;
+				cout<<"Cancelling..."<<endl;
+				return;
+			}
+			else if(currNode->Child != NULL && currNode->Child->getAge() == inputMember->getAge()){
+				cout<<"You cannot marry someone the same age as your children."<<endl;
+				cout<<"Cancelling..."<<endl;
+				return;
+			}
+			
 			
 			if(currNode->Spouse != NULL){
 				cout<<currNode->name<<" has re-married to "<< inputMember->name <<endl;
@@ -99,12 +117,23 @@ class familyTree{
 		}
 		
 		void addChild(int Id, familyMemberNode *inputMember){
-			nextId++;
 			
 			
 			familyMemberNode *currNode = this->getMemberById(Id);
 			familyMemberNode *temp;
 			int oldestChange = 0;
+			
+			//Check children aren't older than parents
+			if( (currNode->getAge() < inputMember->getAge()) || (currNode->Spouse != NULL && currNode->Spouse->getAge() < inputMember->getAge()) ){
+				cout<<"Children cannot be older than their parents."<<endl;
+				cout<<"Cancelling..."<<endl;
+				return;
+			}
+			else if( (currNode->getAge() == inputMember->getAge()) || (currNode->Spouse != NULL && currNode->Spouse->getAge() == inputMember->getAge()) ){
+				cout<<"Children cannot be the same age as their parents."<<endl;
+				cout<<"Cancelling..."<<endl;
+				return;
+			}
 			
 			//compare children.
 			inputMember->Sibling = currNode->Child;
@@ -148,6 +177,62 @@ class familyTree{
 			if(oldestChange == 0){ //If inputMember is the oldest, make them the first child of currNode
 				currNode->Child = inputMember;
 			}
+			nextId++;
+			
+		}
+		
+		void updateMember(int id){
+			string name;
+			int birthYear, birthMonth, birthDay;
+			
+			familyMemberNode *currNode = this->getMemberById(id);
+			int checkLoop = 0;
+			
+			cout<<"Please enter "<< currNode->name << "'s new details: "<<endl;
+			cin.ignore();
+			
+			cout<<"Please enter Name: "<<endl;
+			getline(cin, name);
+			currNode->name = name;
+			
+			do{
+				checkLoop = 0;
+				
+				cout<<"Please enter birth year: "<<endl;
+				cin>>birthYear;
+				currNode->birthYear = birthYear;
+				
+				cout<<"Please enter birth month (in numerical format): "<<endl;
+				cin>>birthMonth;
+				currNode->birthMonth = birthMonth;
+				
+				cout<<"Please enter birth day: "<<endl;
+				cin>>birthDay;
+				currNode->birthDay = birthDay;
+				
+				
+				if(currNode->getAge()<0){
+					cout<<"Error, date hasn't happened yet."<<endl;
+					cout<<endl;
+					checkLoop =1;
+				}
+				if(currNode->Spouse != NULL &&  currNode->getAge()<16 ){
+					cout<<"Current family member is married, You cannot set their age to be below 16.";
+					cout<<endl;
+					checkLoop =1;
+				}
+				if(currNode->Child != NULL &&  currNode->getAge() < currNode->Child->getAge() ){
+					cout<<"Current family member has a child, cannot set their age to be below their child's age.";
+					cout<<endl;
+					checkLoop =1;
+				}
+				else if(currNode->Child != NULL &&  currNode->getAge() == currNode->Child->getAge() ){
+					cout<<"Current family member has a child, cannot set their age to be equal to their child's age.";
+					cout<<endl;
+					checkLoop =1;
+				}
+				
+			}while(checkLoop!=0);
 			
 		}
 		
@@ -203,17 +288,26 @@ familyMemberNode* createFamilyMember(int Id){
 	getline(cin, name);
 	familyMember->name = name;
 	
-	cout<<"Please enter birth year: "<<endl;
-	cin>>birthYear;
-	familyMember->birthYear = birthYear;
-	
-	cout<<"Please enter birth month (in numerical format): "<<endl;
-	cin>>birthMonth;
-	familyMember->birthMonth = birthMonth;
-	
-	cout<<"Please enter birth day: "<<endl;
-	cin>>birthDay;
-	familyMember->birthDay = birthDay;
+	do{
+		cout<<"Please enter birth year: "<<endl;
+		cin>>birthYear;
+		familyMember->birthYear = birthYear;
+		
+		cout<<"Please enter birth month (in numerical format): "<<endl;
+		cin>>birthMonth;
+		familyMember->birthMonth = birthMonth;
+		
+		cout<<"Please enter birth day: "<<endl;
+		cin>>birthDay;
+		familyMember->birthDay = birthDay;
+		
+		
+		if(familyMember->getAge()<0){
+			cout<<"Error, date hasn't happened yet."<<endl;
+			cout<<endl;
+
+		}
+	}while(familyMember->getAge()<0);
 	
 	return familyMember;
 }
@@ -237,8 +331,9 @@ int main()
 	{
 		cout<<"\n 1 Add Spouse "<<endl;
 		cout<<"\n 2 Add Child "<<endl;
-		cout<<"\n 3 Display Nuclear Family "<<endl;
-		cout<<"\n 4 Display Full Family "<<endl;
+		cout<<"\n 3 Update (Update spouse with Add Spouse) "<<endl;
+		cout<<"\n 4 Display Nuclear Family "<<endl;
+		cout<<"\n 5 Display Full Family "<<endl;
 		cout<<"\n 9 Exit "<<endl;
 		cin>>ch;
 		switch(ch)
@@ -248,12 +343,24 @@ int main()
 				cin>>id;
 				if(id>=myFamily.getNextId()){
 					cout<<"Error, Family member doesn't exist"<<endl;
+					cout<<"Cancelling..."<<endl;
+					break;
+				}
+				if(myFamily.getAge(id) < 16){
+					cout<<"A person has to be 16 years or older to get married."<<endl;
+					cout<<"Cancelling..."<<endl;
 					break;
 				}
 				
 				cout<<"Please enter spouse's details: "<<endl;
 				cin.ignore();
 				temp = createFamilyMember(0);
+				
+				if(temp->getAge() < 16){
+					cout<<"A person has to be 16 years or older to get married."<<endl;
+					cout<<"Cancelling..."<<endl;
+					break;
+				}
 				
 				myFamily.addSpouse(id, temp);
 				break;
@@ -263,6 +370,7 @@ int main()
 				cin>>id;
 				if(id>=myFamily.getNextId()){
 					cout<<"Error, Family member doesn't exist"<<endl;
+					cout<<"Cancelling..."<<endl;
 					break;
 				}
 				
@@ -276,15 +384,29 @@ int main()
 			case 3:
 				cout<<"Please enter the id of the targer family member:";
 				cin>>id;
+				
 				if(id>=myFamily.getNextId()){
 					cout<<"Error, Family member doesn't exist"<<endl;
+					cout<<"Cancelling..."<<endl;
+					break;
+				}
+				
+				myFamily.updateMember(id);
+				break;
+				
+			case 4:
+				cout<<"Please enter the id of the targer family member:";
+				cin>>id;
+				if(id>=myFamily.getNextId()){
+					cout<<"Error, Family member doesn't exist"<<endl;
+					cout<<"Cancelling..."<<endl;
 					break;
 				}
 				
 				myFamily.displayNuclearFamily(id);
 				break;
 				
-			case 4:
+			case 5:
 				myFamily.displayFullFamily();
 				break;
 				
